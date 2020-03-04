7158a86bb6066d2d7e3e307688ea04af73260f4b
auideaslab <54638827+auideaslab@users.noreply.github.com>
2020-03-04 09:32:42 -0500

    commented and cleaned code
    This code was heavily commented

--- Assets/GameState.cs
+++ Assets/GameState.cs
@@ -1,46 +1,169 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ *  This class handles game state: it remembers current game state and allows others to check whether a move is valid
+ */
 public class GameState : MonoBehaviour
 {
-    public static Transform[,] chessboard = new Transform[8,8];
-    public GameObject originalSquare;
-    public Transform chessboardParent;
-    public Transform chessPieces;
-    const int SQUARE_SIZE = 2;
-    // Start is called before the first frame update
-    void Start()
+    public static Transform[,] chessboard = new Transform[8,8]; // game state two-dimensiona array
+    public GameObject originalSquare; // reference to square object we use to build the chessboard
+    public Transform chessboardParent; // reference to container of our chessboard
+    public Transform chessPieces; // reference to parent of all pieces
+    const int SQUARE_SIZE = 2; // this constant allow us to scale our chessboard
+
+    // the function below is "static" which means it can be called through class name, like that: GameState.isValidMove
+    // without a need to have a reference to this class. Using "static" is OK only for classes that for sure have just one object
+    // since there is only one game state, it is ok to have "static" functions. Otherwise, it would be a bad idea.
+    public static bool isValidMove(Transform piece, Transform square) // given piece and square, the function checks if piece can move to the square
     {
-        for (int i = 0; i < 8; ++i)
+        if (!piece || !square) return false; // if there is no piece or no square, move is impossible. This should never happen, but better save then sorry!
+
+        if (piece.name.Contains("White")) // validate moves for whites
         {
-            for (int j = 0; j < 8; ++j)
+            if (piece.name.Contains("Pawn")) 
             {
-                GameObject square = Instantiate(originalSquare, chessboardParent);
-                square.GetComponent<Square>().i = i;
-                square.GetComponent<Square>().j = j;
-                square.transform.position = new Vector3(i * SQUARE_SIZE - SQUARE_SIZE * 3.5f, 1.8f, j * SQUARE_SIZE - SQUARE_SIZE * 3.5f);
-
-                for (int k = 0; k < chessPieces.childCount; ++k)
+                if (true)  // replace that with conditions 
                 {
-                    if (Vector3.Distance(chessPieces.GetChild(k).position, square.transform.position) < SQUARE_SIZE/2)
-                    {
-                        square.GetComponent<Square>().piece = chessPieces.GetChild(k);
-                    }
+                    return true;
                 }
-                square.GetComponent<MeshRenderer>().material.color = new Color(1, 0, 0, 0.0f);
+                else return false;
+            } 
+           
 
-                chessboard[i, j] = square.transform;
+            if (piece.name.Contains("King")) 
+            {
+                if (true)  // replace that with conditions 
+                {
+                    return true;
+                }
+                else return false;
+            }
+
+            if (piece.name.Contains("Queen"))
+            {
+                if (true)  // replace that with conditions 
+                {
+                    return true;
+                }
+                else return false;
+            }
+
+            if (piece.name.Contains("Bishop"))
+            {
+                if (true)  // replace that with conditions 
+                {
+                    return true;
+                }
+                else return false;
+            }
+
+            if (piece.name.Contains("Rook"))
+            {
+                if (true)  // replace that with conditions 
+                {
+                    return true;
+                }
+                else return false;
+            }
+
+            if (piece.name.Contains("Horse"))
+            {
+                if (true)  // replace that with conditions 
+                {
+                    return true;
+                }
+                else return false;
+            }
+        }
+        if (piece.name.Contains("Black")) // validate moves for blacks
+        {
+            if (piece.name.Contains("Pawn"))
+            {
+                if (true)  // replace that with conditions 
+                {
+                    return true;
+                }
+                else return false;
+            }
+
+
+            if (piece.name.Contains("King"))
+            {
+                if (true)  // replace that with conditions 
+                {
+                    return true;
+                }
+                else return false;
+            }
+
+            if (piece.name.Contains("Queen"))
+            {
+                if (true)  // replace that with conditions 
+                {
+                    return true;
+                }
+                else return false;
+            }
+
+            if (piece.name.Contains("Bishop"))
+            {
+                if (true)  // replace that with conditions 
+                {
+                    return true;
+                }
+                else return false;
+            }
+
+            if (piece.name.Contains("Rook"))
+            {
+                if (true)  // replace that with conditions 
+                {
+                    return true;
+                }
+                else return false;
+            }
+
+            if (piece.name.Contains("Horse"))
+            {
+                if (true)  // replace that with conditions 
+                {
+                    return true;
+                }
+                else return false;
             }
         }
 
-        Destroy(originalSquare);
-
+        return false;
     }
 
-    // Update is called once per frame
-    void Update()
+    // at the beginning of the game, we procedurally construct the chessboard
+    void Start()
     {
-        
+        for (int i = 0; i < 8; ++i) // in every row
+        {
+            for (int j = 0; j < 8; ++j) // and every column of the chessboard
+            {
+                GameObject square = Instantiate(originalSquare, chessboardParent); // create square
+                square.GetComponent<Square>().i = i; // let square remember it's row
+                square.GetComponent<Square>().j = j; // let square remember it's column
+                square.transform.position = new Vector3(i * SQUARE_SIZE - SQUARE_SIZE * 3.5f, 1.8f, j * SQUARE_SIZE - SQUARE_SIZE * 3.5f); // position the square
+
+                for (int k = 0; k < chessPieces.childCount; ++k) // look through all pieces
+                {
+                    if (Vector3.Distance(chessPieces.GetChild(k).position, square.transform.position) < SQUARE_SIZE/2) // and if piece is closer than half the size of square
+                    {
+                        square.GetComponent<Square>().piece = chessPieces.GetChild(k); // let square remember what piece is sitting on it
+                        chessPieces.GetChild(k).parent = square.transform; // let piece remember what square it is sitting on (by using parent)
+                    }
+                }
+                square.GetComponent<MeshRenderer>().material.color = new Color(0, 0, 0, 0.0f); // reset color to transparent
+
+                chessboard[i, j] = square.transform; // put the square reference into our game state memory at a proper position
+            }
+        }
+
+        Destroy(originalSquare); // once we created all squares, we don't need our initial red square
+
     }
 }
--- Assets/InputController.cs
+++ Assets/InputController.cs
@@ -1,90 +1,110 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+ *  This class handles player's input, such as selection of pieces and squares and moving the pieces
+ */
 public class InputController : MonoBehaviour
 {
- 
-    Transform selectedPiece;
-    Transform selectedSquare;
 
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    Transform selectedPiece; // reference to the selected piece, initially empty
+    Transform selectedSquare; // reference to the selected square, initially empty;
 
     void selectSquare(Transform square)
     {
-        deselectSquare();
-        selectedSquare = square;
-        selectedSquare.GetComponent<MeshRenderer>().material.color = new Color(1, 0, 0, 0.3f);
+        deselectSquare(); //  make sure that previously selected square is deselected
+        selectedSquare = square; // remember the selected square
+        highlightValid(); // highlight newly selected square
     }
 
     void deselectSquare()
     {
-        if (selectedSquare)
+        if (selectedSquare) // deselecting is only possible if a square was first selected
         {
-            selectedSquare.GetComponent<MeshRenderer>().material.color = new Color(0, 0, 0, 0.0f);
+            selectedSquare.GetComponent<MeshRenderer>().material.color = new Color(0, 0, 0, 0.0f); //change color to transparent (alpha = 0)
+            selectedSquare = null; // forget the previosly selected square
         }
+    }
+
+    void highlightValid()
+    {
+        selectedSquare.GetComponent<MeshRenderer>().material.color = new Color(0, 1, 0, 0.3f); // highlight with green color
+    }
+
+    void highlightInvalid()
+    {
+        selectedSquare.GetComponent<MeshRenderer>().material.color = new Color(1, 0, 0, 0.3f); // highlight with red color
     }
 
     void selectPiece(Transform piece)
     {
-        deselectPiece();
-        selectedPiece = piece;
+        deselectPiece(); // first deselect the previosly selected piece, if any
+        selectedPiece = piece; // remember selected piece
 
-        Renderer[] renderers = selectedPiece.GetComponentsInChildren<Renderer>();
-        foreach (Renderer r in renderers)
+        Renderer[] renderers = selectedPiece.GetComponentsInChildren<Renderer>(); // get renderers of all visual elements of the selected piece
+        foreach (Renderer r in renderers) // iterate over all renderers collected in the previous step
         {
-            r.material.EnableKeyword("_EMISSION");
-            r.material.SetColor("_EmissionColor", Color.red);
+            r.material.EnableKeyword("_EMISSION"); // enable light emission in the shader
+            r.material.SetColor("_EmissionColor", Color.green); // set light emission to green
         }
     }
 
     void deselectPiece()
     {
-        if (selectedPiece)
+        if (selectedPiece) // make sure that there is a piece to be deselected
         {
-            Renderer[] renderers = selectedPiece.GetComponentsInChildren<Renderer>();
-            foreach (Renderer r in renderers)
+            Renderer[] renderers = selectedPiece.GetComponentsInChildren<Renderer>(); // get renderers of all visual elements of the selected piece
+            foreach (Renderer r in renderers) // iterate over all renderers in collected in the previous step
             {
-                r.material.DisableKeyword("_EMISSION");
-                r.material.SetColor("_EmissionColor", Color.black);
+                r.material.DisableKeyword("_EMISSION"); // disable light emission in the shader
+                r.material.SetColor("_EmissionColor", Color.black); // set light emission color to black
             }
         }
-        selectedPiece = null;
+        selectedPiece = null; // forget the piece
     }
+
+    void movePiece()
+    {
+        if (selectedSquare && selectedPiece) // check if there is piece to move and the destination for it was selected
+        {
+            selectedPiece.position = selectedSquare.position; // move the piece. Later, we will animate this step, but for now the move is immediate
+            selectedSquare.GetComponent<Square>().piece = selectedPiece; // let square remember what piece is sitting on it
+            selectedPiece.parent = selectedSquare; // let piece remember was piece it is sitting on, by setting it as parent
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
 
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // create an infinite ray from camera, thorough screen, into the game word
 
-            RaycastHit hit;
+        RaycastHit hit; // this object will be filled up with whatever the ray hits
 
-            if (Physics.Raycast(ray, out hit, 100))
+        if (Physics.Raycast(ray, out hit, 100)) // perform the ray casting test. If anything was hit, it will be remembered in "hit" object
+        {
+            if (hit.transform.tag == "square" && selectedPiece) // check if the tag of the object we hit is "square"
             {
-                if (hit.transform.tag == "square" && selectedPiece)
+                selectSquare(hit.transform); // select the object that was hit (since we now know it is "square"). For now, we assume the square is a valid move
+                if (GameState.isValidMove(selectedPiece, selectedSquare))
                 {
-                    selectSquare(hit.transform);
-                    if (Input.GetMouseButtonDown(0))
+                    if (Input.GetMouseButtonDown(0)) // check if the mouse button was pressed and whether the move is valid 
                     {
-                        selectedPiece.position = hit.transform.position;
-                        deselectPiece();
+                        movePiece(); // move the piece
+                        deselectPiece(); // deselected the piece after it was moved
                     }
-                 
                 }
-
-                if (Input.GetMouseButtonDown(0) && hit.transform.tag == "piece")
-                
+                else
                 {
-
-                    selectPiece(hit.transform);
-
+                    highlightInvalid(); // in selectSquare, we assumed the the move is valid, but now we know it is not, so we highlight the square as invalid
                 }
             }
+        }
 
+        if (Input.GetMouseButtonDown(0) && hit.transform.tag == "piece") // if we clicked on a piece
+        {
+            selectPiece(hit.transform); // then we select that piece
+        }
     }
+
 }
